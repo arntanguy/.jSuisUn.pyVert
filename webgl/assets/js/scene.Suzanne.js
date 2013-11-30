c57f36ef440812576e7ccb795c86264f431fc208
TANGUY Arnaud <arn.tanguy@gmail.com>
2013-11-30 19:38:36 +0100

    Load blender scene :)

--- webgl/assets/js/scene.Suzanne.js
+++ webgl/assets/js/scene.Suzanne.js
@@ -1,142 +1,20 @@
 {
 
-"metadata" :
-{
-	"formatVersion" : 3.2,
-	"type"          : "scene",
-	"sourceFile"    : "scene.blend",
-	"generatedBy"   : "Blender 2.65 Exporter",
-	"objects"       : 4,
-	"geometries"    : 2,
-	"materials"     : 2,
-	"textures"      : 0
-},
-
-"urlBaseType" : "relativeToScene",
-
-
-"objects" :
-{
-	"Suzanne" : {
-		"geometry"  : "geo_Suzanne",
-		"groups"    : [  ],
-		"material"  : "Material.001",
-		"position"  : [ -0.799487, -0.423912, 2.33256 ],
-		"rotation"  : [ 7.79399e-08, 0, -0 ],
-		"quaternion": [ 3.897e-08, 0, 0, 1 ],
-		"scale"     : [ 1, 1, 1 ],
-		"visible"       : true,
-		"castShadow"    : false,
-		"receiveShadow" : false,
-		"doubleSided"   : false
+	"metadata" :
+	{
+		"formatVersion" : 3.1,
+		"generatedBy"   : "Blender 2.65 Exporter",
+		"vertices"      : 507,
+		"faces"         : 500,
+		"normals"       : 507,
+		"colors"        : 0,
+		"uvs"           : [],
+		"materials"     : 1,
+		"morphTargets"  : 0,
+		"bones"         : 0
 	},
 
-	"Cube" : {
-		"geometry"  : "geo_Cube.001",
-		"groups"    : [  ],
-		"material"  : "Material",
-		"position"  : [ 0, 0, 0 ],
-		"rotation"  : [ -1.5708, 0, 0 ],
-		"quaternion": [ -0.707107, 0, 0, 0.707107 ],
-		"scale"     : [ 1, 1, 1 ],
-		"visible"       : true,
-		"castShadow"    : false,
-		"receiveShadow" : false,
-		"doubleSided"   : false
-	},
-
-	"Lamp" : {
-		"type"       : "PointLight",
-		"position"   : [ 4.07625, 5.90386, 1.00545 ],
-		"rotation"   : [ 0.650328, 1.86639, 0.0552171 ],
-		"color"      : 16777215,
-		"distance"   : 30.000,
-		"intensity"  : 4.960
-	},
-
-	"Camera" : {
-		"type"  : "PerspectiveCamera",
-		"fov"   : 49.159264,
-		"aspect": 1.333000,
-		"near"  : 0.100000,
-		"far"   : 100.000000,
-		"position": [ 7.48113, 5.34367, 6.50764 ],
-		"target"  : [ 0, 0, 0 ]
-	}
-},
-
-
-"geometries" :
-{
-	"geo_Suzanne" : {
-		"type" : "embedded",
-		"id"  : "emb_Suzanne"
-	},
-
-	"geo_Cube.001" : {
-		"type" : "embedded",
-		"id"  : "emb_Cube.001"
-	}
-},
-
-
-"materials" :
-{
-	"Material" : {
-		"type": "MeshLambertMaterial",
-		"parameters": { "color": 1256099, "ambient": 1256099, "opacity": 1, "blending": "NormalBlending" }
-	},
-
-	"Material.001" : {
-		"type": "MeshLambertMaterial",
-		"parameters": { "color": 10682457, "ambient": 10682457, "opacity": 1, "blending": "NormalBlending" }
-	}
-},
-
-
-"embeds" :
-{
-"emb_Cube.001": {	"scale" : 1.000000,
-
-	"materials" : [	{
-		"DbgColor" : 15658734,
-		"DbgIndex" : 0,
-		"DbgName" : "Material",
-		"blending" : "NormalBlending",
-		"colorAmbient" : [0.07621849292842287, 0.16522515066303534, 0.6400000190734865],
-		"colorDiffuse" : [0.07621849292842287, 0.16522515066303534, 0.6400000190734865],
-		"colorSpecular" : [0.5, 0.5, 0.5],
-		"depthTest" : true,
-		"depthWrite" : true,
-		"shading" : "Lambert",
-		"specularCoef" : 50,
-		"transparency" : 1.0,
-		"transparent" : false,
-		"vertexColors" : false
-	}],
-
-	"vertices" : [1,1,-1,1,-1,-1,-1,-1,-1,-1,1,-1,1,0.999999,1,0.999999,-1,1,-1,-1,1,-1,1,1],
-
-	"morphTargets" : [],
-
-	"normals" : [0.577349,0.577349,-0.577349,0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,-0.577349,0.577349,-0.577349,0.577349,0.577349,0.577349,-0.577349,0.577349,0.577349,-0.577349,-0.577349,0.577349,0.577349,-0.577349,0.577349],
-
-	"colors" : [],
-
-	"uvs" : [],
-
-	"faces" : [35,0,1,2,3,0,0,1,2,3,35,4,7,6,5,0,4,5,6,7,35,0,4,5,1,0,0,4,7,1,35,1,5,6,2,0,1,7,6,2,35,2,6,7,3,0,2,6,5,3,35,4,0,3,7,0,4,0,3,5],
-
-	"bones" : [],
-
-	"skinIndices" : [],
-
-	"skinWeights" : [],
-
-	"animations" : []
-},
-
-"emb_Suzanne": {	"scale" : 1.000000,
+	"scale" : 1.000000,
 
 	"materials" : [	{
 		"DbgColor" : 15658734,
@@ -174,22 +52,6 @@
 	"skinWeights" : [],
 
 	"animations" : []
-}
-},
 
-
-"transform" :
-{
-	"position"  : [ 0, 0, 0 ],
-	"rotation"  : [ 0, 0, 0 ],
-	"scale"     : [ 1, 1, 1 ]
-},
-
-"defaults" :
-{
-	"bgcolor" : [ 0, 0, 0 ],
-	"bgalpha" : 1.000000,
-	"camera"  : "Camera"
-}
 
 }
