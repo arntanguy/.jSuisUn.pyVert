bc81f4abb5d01c48b3d71379d2e794b24ab13fe2
TANGUY Arnaud <arn.tanguy@gmail.com>
2013-12-04 21:52:34 +0100

    FPS controls (part 1: example)

--- webgl/assets/js/scene.js
+++ webgl/assets/js/scene.js
@@ -6,9 +6,9 @@
 	"type"          : "scene",
 	"sourceFile"    : "scene.blend",
 	"generatedBy"   : "Blender 2.65 Exporter",
-	"objects"       : 7,
-	"geometries"    : 4,
-	"materials"     : 3,
+	"objects"       : 4,
+	"geometries"    : 2,
+	"materials"     : 2,
 	"textures"      : 0
 },
 
@@ -17,64 +17,32 @@
 
 "objects" :
 {
-	"Plane.001" : {
-		"geometry"  : "geo_Plane.001",
+	"Plane" : {
+		"geometry"  : "geo_Plane.002",
 		"groups"    : [  ],
-		"material"  : "Material.001",
-		"position"  : [ 1.21357, -0.428967, 12.0661 ],
+		"material"  : "Material.004",
+		"position"  : [ -3.79895, -1.16511, -0.0210904 ],
 		"rotation"  : [ -1.5708, 0, 0 ],
 		"quaternion": [ -0.707107, 0, 0, 0.707107 ],
-		"scale"     : [ 8.42497, 8.42496, 8.42496 ],
+		"scale"     : [ 6.43443, 6.43443, 6.43443 ],
 		"visible"       : true,
 		"castShadow"    : false,
-		"receiveShadow" : false,
+		"receiveShadow" : true,
 		"doubleSided"   : false,
-                "physicsShape"  : "PlaneMesh",
-		"physicsMass"   : 0.0
-	},
-
-	"Suzanne" : {
-		"geometry"  : "geo_Suzanne",
-		"groups"    : [  ],
-		"material"  : "Material.002",
-		"position"  : [ 1.24655, 3.12385, -2.33983 ],
-		"rotation"  : [ 7.79399e-08, 0, -0 ],
-		"quaternion": [ 3.897e-08, 0, 0, 1 ],
-		"scale"     : [ 0.690827, 0.690827, 0.690827 ],
-		"visible"       : true,
-		"castShadow"    : false,
-		"receiveShadow" : false,
-		"doubleSided"   : false,
-                "physicsShape"  : "SphereMesh",
-		"physicsMass"   : 0.0771484375
-	},
-
-	"Plane" : {
-		"geometry"  : "geo_Plane",
-		"groups"    : [  ],
-		"material"  : "Material.001",
-		"position"  : [ 1.21357, 1.23282, -1.80915 ],
-		"rotation"  : [ -1.51948, 0, 0 ],
-		"quaternion": [ -0.688734, 0, 0, 0.725015 ],
-		"scale"     : [ -6.76114, -6.76114, -6.76114 ],
-		"visible"       : true,
-		"castShadow"    : false,
-		"receiveShadow" : false,
-		"doubleSided"   : false,
-                "physicsShape"  : "PlaneMesh",
+                "physicsShape"  : "BoxMesh",
 		"physicsMass"   : 0.0
 	},
 
 	"Cube" : {
-		"geometry"  : "geo_Cube.001",
+		"geometry"  : "geo_Cube.003",
 		"groups"    : [  ],
-		"material"  : "Material",
-		"position"  : [ 1.92323, 3.46272, -3.34103 ],
+		"material"  : "Material.003",
+		"position"  : [ 0.000573635, -6.74134e-09, 0.196951 ],
 		"rotation"  : [ -1.5708, 0, 0 ],
 		"quaternion": [ -0.707107, 0, 0, 0.707107 ],
-		"scale"     : [ 0.690827, 0.690827, 0.690827 ],
+		"scale"     : [ 1, 1, 1 ],
 		"visible"       : true,
-		"castShadow"    : false,
+		"castShadow"    : true,
 		"receiveShadow" : false,
 		"doubleSided"   : false,
                 "physicsShape"  : "BoxMesh",
@@ -83,19 +51,10 @@
 
 	"Sun" : {
 		"type"       : "AmbientLight",
-		"position"   : [ -14.3081, 11.4284, -1.52643 ],
-		"rotation"   : [ 0, 0, 0 ],
+		"position"   : [ 6.60006, 5.91994, 8.01279 ],
+		"rotation"   : [ -1.83964e-08, -3.99724e-08, 0.862654 ],
 		"color"      : 16777215,
 		"distance"   : 25.000,
-		"intensity"  : 1.000
-	},
-
-	"Lamp" : {
-		"type"       : "PointLight",
-		"position"   : [ 4.07625, 5.90386, 1.00545 ],
-		"rotation"   : [ 0.650328, 1.86639, 0.0552171 ],
-		"color"      : 16777215,
-		"distance"   : 30.000,
 		"intensity"  : 1.000
 	},
 
@@ -105,7 +64,7 @@
 		"aspect": 1.333000,
 		"near"  : 0.100000,
 		"far"   : 100.000000,
-		"position": [ 7.22805, 5.77115, 18.8399 ],
+		"position": [ 7.48113, 5.34367, 6.50764 ],
 		"target"  : [ 0, 0, 0 ]
 	}
 },
@@ -113,43 +72,28 @@
 
 "geometries" :
 {
-	"geo_Plane.001" : {
+	"geo_Plane.002" : {
 		"type" : "ascii",
-		"url"  : "scene.Plane.001.js"
+		"url"  : "scene.Plane.002.js"
 	},
 
-	"geo_Suzanne" : {
+	"geo_Cube.003" : {
 		"type" : "ascii",
-		"url"  : "scene.Suzanne.js"
-	},
-
-	"geo_Plane" : {
-		"type" : "ascii",
-		"url"  : "scene.Plane.js"
-	},
-
-	"geo_Cube.001" : {
-		"type" : "ascii",
-		"url"  : "scene.Cube.001.js"
+		"url"  : "scene.Cube.003.js"
 	}
 },
 
 
 "materials" :
 {
-	"Material" : {
+	"Material.003" : {
 		"type": "MeshLambertMaterial",
-		"parameters": { "color": 1418052, "ambient": 1418052, "opacity": 1, "blending": "NormalBlending" }
+		"parameters": { "color": 7995811, "ambient": 7995811, "opacity": 1, "blending": "NormalBlending" }
 	},
 
-	"Material.001" : {
-		"type": "MeshLambertMaterial",
-		"parameters": { "color": 826019, "ambient": 826019, "opacity": 1, "blending": "NormalBlending" }
-	},
-
-	"Material.002" : {
-		"type": "MeshLambertMaterial",
-		"parameters": { "color": 10682493, "ambient": 10682493, "opacity": 1, "blending": "NormalBlending" }
+	"Material.004" : {
+		"type": "MeshPhongMaterial",
+		"parameters": { "color": 3449600, "ambient": 3449600, "opacity": 1, "ambient": 3449600, "specular": 8355711, "shininess": 5e+01, "blending": "NormalBlending" }
 	}
 },
 
